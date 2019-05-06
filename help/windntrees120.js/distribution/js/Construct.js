0ce7862272e88ea6a7febf7b21b6a2502601ce13
shamszia <shams@invincibletec.com>
2019-05-06 12:06:49 +0500

    rain help documents
    Help documents.

---
+++ help/windntrees120.js/distribution/js/Construct.js
@@ -0,0 +1,16 @@
+﻿function Construct(data) {
+    var instance = this;
+    instance._datakey = data.Id;
+    instance.Id = data.Id;
+
+    instance.Scope = data.Scope;
+    instance.Constraint = data.Constraint;
+    instance.Partial = data.Partial;
+    instance.Type = data.Type;
+    instance.Name = data.Name;
+    instance.Description = data.Description;
+    instance.Comments = data.Comments;
+    instance.Annotations = data.Annotations;
+
+    instance.Constructs = data.Constructs;
+}
